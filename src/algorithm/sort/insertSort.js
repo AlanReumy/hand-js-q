6f0779891d5368f8d14def1061959d0a53f9760f
ziming.tang <ziming.tang@intchains.com>
2023-03-09 15:33:56 +0800

    feat: insertSort

---
+++ src/algorithm/sort/insertSort.js
@@ -0,0 +1,12 @@
+function insertSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let j = i;
+    let target = arr[j];
+    while (j > 0 && arr[j - 1] > target) {
+      arr[j] = arr[j - 1];
+      j--;
+    }
+    arr[j] = target;
+  }
+  return arr;
+}
