a7ac8dba887ef37dd091c00d05d2b7e934e86df3
ziming.tang <ziming.tang@intchains.com>
2023-03-09 16:18:14 +0800

    feat: mergeSort

---
+++ src/algorithm/sort/mergeSort.js
@@ -0,0 +1,31 @@
+function merge(left, right) {
+  let i = 0;
+  let j = 0;
+  const res = [];
+  while (i < left.length && j < right.length) {
+    if (left[i] < right[j]) {
+      res.push(left[i]);
+      i++;
+    } else {
+      res.push(right[j]);
+      j++;
+    }
+  }
+  if (i < left.length) {
+    res.push(...left.slice(i));
+  } else {
+    res.push(...right.slice(j));
+  }
+  return res;
+}
+
+function mergeSort(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
+
+  const mid = Math.floor(arr.length / 2);
+  const left = mergeSort(arr.slice(0, mid));
+  const right = mergeSort(arr.slice(mid));
+  return merge(left, right);
+}
